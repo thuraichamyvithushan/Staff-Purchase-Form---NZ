5318fa35a2309f3c9a24503e441fc66e7d85cb69
thuraichamyvithushan <145326510+thuraichamyvithushan@users.noreply.github.com>
2026-02-25 09:42:02 +0530

    backend vercel url added

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
 export const API_BASE_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
     ? 'http://localhost:5001'
-    : 'https://staff-purchase-form.vercel.app';
+    : 'https://staff-purchase-form-nz.vercel.app';
